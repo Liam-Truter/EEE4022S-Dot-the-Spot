873aafd41087f9cd3360ecbee11d51b22f880327
Liam-Truter <126420680+Liam-Truter@users.noreply.github.com>
2024-09-18 11:07:22 +0200

    Created test basic interface with 3d visualisation.

---
+++ Robot Interface/test.py
@@ -0,0 +1,43 @@
+from opentrons import robot, instruments, containers
+
+from ursina import *
+
+def update():
+    head_pos = robot._driver.get_head_position()['current']
+    head_x = head_pos.coordinates.x
+    head_y = head_pos.coordinates.y
+    head_z = head_pos.coordinates.z
+    robot_head.x = head_x
+    robot_head.y = head_y
+    robot_head.z = head_z
+    print(head_pos)
+
+    dx = 100 * time.dt
+
+    # Move along the x-axis
+    try:
+        if held_keys['d'] and not held_keys['a'] and head_x+dx<=400:
+            robot.move_head(x=head_x+dx)
+        elif held_keys['a'] and not held_keys['d']:
+            robot.move_head(x=head_x-dx)
+    except:
+        # Cancel movement if limit switch hit
+        print("X Limit Switch Hit!")
+        robot.move_head(x=head_x)
+
+    # Move along the y-axis
+    try:
+        if held_keys['w'] and not held_keys['s']:
+            robot.move_head(y=head_y+dx)
+        elif held_keys['s'] and not held_keys['w'] and head_y-dx>=0:
+            robot.move_head(y=head_y-dx)
+    except:
+        # Cancel movement if limit switch hit
+        print("Y Limit Switch Hit!")
+        robot.move_head(y=head_y)
+
+app = Ursina()
+robot.connect("Virtual Smoothie")
+camera.position=(200,200,-1000)
+robot_head = Entity(model="cube", color=color.red, texture="white_cube", scale=40)
+app.run()
